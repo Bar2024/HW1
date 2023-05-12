6a044130ff2316469737bab89aa4c16223a75ca3
Sofia Torgovezky <sofiat@campus.technion.ac.il>
2023-05-12 19:18:21 +0300

    11/05: -created the Action class. -wrote part of methods in Action and State.
    12/05:
    -new class enumDirection
    -changed from class to enum (a special class)

--- src/EnumDirection.java
+++ src/EnumDirection.java
@@ -1,11 +1,9 @@
-public class EnumDirection {
+/**
+ * class that can have one of the next direction: up, down, right, left
+ */
+public enum EnumDirection {
     // modify to the specific way of the board
     // possible direction are: UP, DOWN, RIGHT, LEFT
     // should this class contain only one string that will be the direction?
-    final String UP = "UP";
-    final String DOWN = "DOWN";
-    final String RIGHT = "RIGHT";
-    final String LEFT = "LEFT";
-
-
+    UP, DOWN, RIGHT, LEFT;
 }
