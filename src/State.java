da8aa12fcbabb7bb99ba530da753d9d1aa79daf3
Sofia Torgovezky <sofiat@campus.technion.ac.il>
2023-05-12 21:02:42 +0300

    11/05: -created the Action class. -wrote part of methods in Action and State.
    12/05:
    -new class enumDirection
    -changed from class to enum (a special class)
    -no more problems with Action class
    -method result has been written in state(no final)

--- src/State.java
+++ src/State.java
@@ -1,22 +1,23 @@
-import java.lang.reflect.Array;
-
 public class State {
     static Board board;
     final Board WANTED_BOARD;
-    public String Action[];
+    static EnumDirection direction;
+    public String possibleAction[];
 
     /**
      * The constructor for Board.
      *
      * @param board The current state of the board
      */
-    public State(Board board) {
+    public State(Board board, EnumDirection direction) {
         this.board = board;
         WANTED_BOARD = board.getWantedBoard();
+        this.direction = direction;
     }
 
     /**
      * checks if the current state of the board is the wanted state
+     *
      * @return true if the both boards are equals, false otherwise
      */
     public boolean isGoal() {
@@ -27,11 +28,6 @@ public class State {
     }
 
     public String[] actions() {
-
-
-    }
-
-    public State result(Action) {
 
 
     }
@@ -48,5 +44,28 @@ public class State {
     @Override
     public int hashCode() {
         return board.hashCode();
+    }
+
+    // change the next method according to the way the board is madee
+    public Board result(Action action) {
+        Tile tile = action.getTileAction();
+        EnumDirection direction = action.getDirection();
+        // GET THE INDEX OF THE WANTED TILE
+        int n;
+        int m;
+        if (direction == EnumDirection.UP) {
+            board[n - 1][m] = board[n][m];
+            board[n][m] = "_";
+        } else if (direction == EnumDirection.DOWN) {
+            board[n + 1][m] = board[n][m];
+            board[n][m] = "_";
+        } else if (direction == EnumDirection.RIGHT) {
+            board[n][m + 1] = board[n][m];
+            board[n][m] = "_";
+        } else if (direction == EnumDirection.LEFT) {
+            board[n][m - 1] = board[n][m];
+            board[n][m] = "_";
+        }
+        return board;
     }
 }
